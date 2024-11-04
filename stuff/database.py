acd1c0fd16f36a026f22c9323a53d7ac4b1b9f41
Xalililloh Nazarov <167248708+belikemycat@users.noreply.github.com>
2024-11-04 21:19:20 +0500

    Typo

--- stuff/database.py
+++ stuff/database.py
@@ -2,13 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./movies.db"  # Путь к файлу базы данных SQLite
+DATABASE_URL = "sqlite:///./movies.db"  
 
-# создаем к движку по url
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-# настраиваем сессии для подключение к дб
+# session to connect to database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Базовый класс для создания моделей базы данных
+# base class for creating the database
 Base = declarative_base()
